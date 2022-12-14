db77e5d1ad940b54ad9f7da2c1a7a39239230fed
Pavel.Balozhka <Pavel.Balozhka@itechart-group.com>
2022-12-14 22:34:06 +0300

    fix: fixed router wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {Layout} from "./pages/Layout";
 import {BasketPage} from "./pages/busket/BasketPage";
 import {CatalogPage} from "./pages/catalog/CatalogPage";
@@ -8,14 +8,14 @@ import {CatalogPage} from "./pages/catalog/CatalogPage";
 function App() {
   return (
       <div className="app">
-          <BrowserRouter>
+          <Router>
               <Routes>
                   <Route path="/" element={<Layout />}>
                       <Route path="basket" element={<BasketPage />} />
                       <Route path="catalog" element={<CatalogPage />} />
                   </Route>
               </Routes>
-          </BrowserRouter>
+          </Router>
       </div>
   );
 }
